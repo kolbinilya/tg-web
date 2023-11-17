25d976f1a9441b141a5761798476181fe3c34565
ilakolbin <kolbinilya93@gmail.com>
2023-11-17 13:28:59 +0400

    button close

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import {useEffect} from "react";
+
+const tg = window.Telegram.WebApp
+
 
 function App() {
+  const url = 'https://www.licious.in/blog/wp-content/uploads/2022/06/shutterstock_1339636625-1.jpg'
+
+  useEffect(() => {
+    tg.ready()
+  }, []);
+
+  const onClose = () => {
+    tg.close()
+  }
+
   return (
     <div className="App">
       <h1>hello react</h1>
-      <h1>hello react</h1>
-      <button>close</button>
+      <button className='closeBtn' onClick={onClose}>close</button>
+      <div className='container'>
+        <img className='img' src={url} alt=""/>
+      </div>
     </div>
   );
 }
