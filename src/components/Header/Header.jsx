04cd10b6a501a32022e35774bae1dd9f0b758dcb
ilakolbin <kolbinilya93@gmail.com>
2023-11-17 14:54:38 +0400

    adasdad

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
 			<div className={'header'}>
 				<Button onClick={onClose}>Close</Button>
 				<span className={'username'}>
-					{tg.initDataUnsafe?.user?.username}
+					{tg.initData}
 				</span>
 			</div>
 	);
