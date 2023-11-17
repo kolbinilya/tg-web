679073a9320a8992d6a48442b658440f778b48fd
ilakolbin <kolbinilya93@gmail.com>
2023-11-17 14:38:44 +0400

    toggle and header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import './App.css';
 import {useEffect} from "react";
-
-const tg = window.Telegram.WebApp
+import Header from "./components/Header/Header";
+import {useTelegram} from "./hooks/useTelegram";
 
 
 function App() {
-  const url = 'https://www.licious.in/blog/wp-content/uploads/2022/06/shutterstock_1339636625-1.jpg'
+	const {tg, onToggleButton} = useTelegram()
+	const url = 'https://www.licious.in/blog/wp-content/uploads/2022/06/shutterstock_1339636625-1.jpg'
 
-  useEffect(() => {
-    tg.ready()
-  }, []);
+	useEffect(() => {
+		tg.ready()
+	}, []);
 
-  const onClose = () => {
-    tg.close()
-  }
-
-  return (
-    <div className="App">
-      <h1>hello react</h1>
-      <button className='closeBtn' onClick={onClose}>close</button>
-      <div className='container'>
-        <img className='img' src={url} alt=""/>
-      </div>
-    </div>
-  );
+	return (
+			<div className="App">
+				<Header/>
+				<div className='container'>
+					<img className='img' src={url} alt=""/>
+				</div>
+				<button onClick={onToggleButton}>toggle</button>
+			</div>
+	);
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from "../Button/Button";
+import {useTelegram} from "../../hooks/useTelegram";
+
+
+const Header = () => {
+	const {onClose, user} = useTelegram()
+
+	return (
+			<div className={'header'}>
+				<Button onClick={onClose}>Close</Button>
+				<span className={'username'}>
+					{user?.username}
+				</span>
+			</div>
+	);
+};
+
+export default Header;
